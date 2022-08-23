061884a45f004fc74aa5caedc7f5725788035433
HaSanMorshed99 <hasanmorshed.bsmrstucse@gmail.com>
2022-08-23 11:09:23 +0600

    f

---
+++ budget.js
@@ -0,0 +1,6 @@
+document.getElementById('bclc').addEventListener('click', function(){
+    const perplri=document.getElementById('ipercost');
+    const plramountstr=perplri.value;
+    const newperplri=parseFloat(plramountstr);
+
+})
